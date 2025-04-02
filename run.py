5514e9cc07b5e0586437a0504fbb53e569ac2803
ProgramCodeMasterA <vb7zl582@students.codeinstitute.net>
2025-04-02 10:33:30 +0100

    Add BattleshipGame Class object, Add print_board function

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+import random
+
+# configure BattleshipGame Class object
+class BattleshipGame:
+    def __init__(self):
+        self.LENGTH_OF_SHIPS = [2, 3, 3, 4, 5]
+        self.PLAYER_BOARD = [[" "] * 10 for _ in range(10)]
+        self.COMPUTER_BOARD = [[" "] * 10 for _ in range(10)]
+        self.PLAYER_GUESS_BOARD = [[" "] * 10 for _ in range(10)]
+        self.COMPUTER_GUESS_BOARD = [[" "] * 10 for _ in range(10)]
+        self.LETTERS_TO_NUMBERS = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9}
+
+    # print current state of current player's board
+    def print_board(self, board):
+        print("  A|B|C|D|E|F|G|H|I|J")
+        print("---------------------")
+        row_number = 0
+        for row in board:
+            print('{}|{}'.format(row_number, '|'.join(row)))
+            row_number += 1
